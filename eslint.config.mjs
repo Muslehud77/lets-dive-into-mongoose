670eb9ea0103b6db7f86a31ec9af1ca49f5e4442
Muslehud77 <fardinmohit@gmail.com>
2024-05-18 18:40:25 +0800

    basic project setup completed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,29 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import js from "@eslint/js";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import js from '@eslint/js';
 
 export default [
   js.configs.recommended,
-  { languageOptions: { globals: globals.node } },
+  {
+    files: ['src/**/*.ts', 'src/**/*.tsx'],
+    ignores: ['.dist/',".node_modules/"],
+    languageOptions: { globals: globals.node },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+
   {
+    
     rules: {
-      "@typescript-eslint/no-unused-vars": "error",
+      '@typescript-eslint/no-unused-vars': 'error',
       // to enforce using type for object type definitions, can be type or interface
-      "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+
+      'no-unused-expressions': 'warn',
+      'prefer-const': 'warn',
+      'no-console': 'warn',
+      'no-undef': 'error',
     },
   },
 ];
---
+++ readme.md
@@ -0,0 +1,56 @@
+
+# Structure of Nodejs Ts and Mongoose
+
+## Setup
+
+1. Clone the repository:
+    ```
+    git clone <repository-url>
+    ```
+
+2. Navigate into the project directory:
+    ```
+    cd <project-name>
+    ```
+
+3. Install the dependencies:
+    ```
+    npm install
+    ```
+
+## Setting up ESLint
+
+This project uses ESLint for linting. The configuration can be found in the `eslint.config.mjs` file.
+
+1. Install ESLint:
+    ```
+    npm install eslint --save-dev
+    ```
+
+2. Run ESLint:
+    ```
+    npm run lint
+    ```
+
+3. Fix ESLint issues:
+    ```
+    npm run lint:fix
+    ```
+
+## Setting up Prettier
+
+This project uses Prettier for code formatting. The configuration can be found in the `.prettierrc.json` file.
+
+1. Install Prettier:
+    ```
+    npm install --save-dev prettier
+    ```
+
+2. Run Prettier:
+    ```
+    npm run prettier
+    ```
+
+## Building the Project
+
+This project uses TypeScript as its main language. You can compile your TypeScript files into JavaScript using the following command:
